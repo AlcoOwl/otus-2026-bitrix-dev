f0db757a4b3fa954ffacb128d4c360f05de5f338
AlcoOwl <maribel0701@gmail.com>
2026-04-13 17:45:19 +0700

    **refactor(homeworks): обновлено описание задания и добавлены ссылки на исходный код**
    - Обновлено текстовое описание ДЗ3: добавлены ссылки на исходный код абстрактного класса и моделей.
    - Переработан формат ссылки на страницу демонстрации функционала, выделен отдельный заголовок.
    - Исправлены неточности в разметке, улучшен внешний вид.

--- local/homeworks/homework3/index.php
+++ local/homeworks/homework3/index.php
@@ -66,17 +66,21 @@ Asset::getInstance()->addCss('//cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bo
 <h4 class="mb-2">Пояснительная записка</h4>
 <div class="mb-4">
     <p>
-        Для выполнения задания был написан абстрактный D7-класс для работы со свойствами инфоблоков: он формирует карту полей,
-        поддерживает одиночные и множественные свойства и позволяет описывать связи через ORM.
-        На его основе созданы модели <b>DoctorsPropertiesTable</b> и <b>ProceduresPropertiesTable</b>, в которых описаны
-        поля врачей (ФИО, стаж, привязка к процедурам) и процедур (цена). В
-        <a href="/local/tests/custom_model.php">/local/tests/custom_model.php</a> и
-        <a href="/local/tests/multi.php">/local/tests/multi.php</a> проверены выборки связанных полей, runtime-связи и работа
-        с множественными свойствами. В самом разделе ДЗ реализованы страница со списком врачей, карточка врача с перечнем
-        его процедур, а также формы добавления врачей, процедур и связей между ними.
-
-        <a href="/local/homeworks/homework3/doctors.php">Страница с демонстрацией функционала &rarr;</a>
+        Для выполнения задания был написан абстрактный D7-класс <a href="https://github.com/AlcoOwl/otus-2026-bitrix-dev/blob/main/local/php_interface/src/Models/AbstractIblockPropertyValuesTable.php">/local/php_interface/src/models/AbstractIblockPropertyValuesTable.php</a><br>
+        Он формирует карту полей, поддерживает одиночные и множественные свойства и позволяет описывать связи через ORM.<br>
+        На его основе созданы модели <a href="https://github.com/AlcoOwl/otus-2026-bitrix-dev/blob/main/local/php_interface/src/Models/DoctorsPropertiesTable.php">/local/php_interface/src/models/DoctorsPropertiesTable.php</a> и
+        <a href="https://github.com/AlcoOwl/otus-2026-bitrix-dev/blob/main/local/php_interface/src/Models/ProceduresPropertiesTable.php">/local/php_interface/src/models/ProceduresPropertiesTable.php</a><br>
+        в которых описаны поля врачей (ФИО, стаж, привязка к процедурам) и процедур (цена).<br>
+        В <a href="https://github.com/AlcoOwl/otus-2026-bitrix-dev/blob/main/local/tests/custom_model.php">/local/tests/custom_model.php</a> и
+        <a href="https://github.com/AlcoOwl/otus-2026-bitrix-dev/blob/main/local/tests/multi.php">/local/tests/multi.php</a> проверены выборки связанных полей, runtime-связи и работа
+        с множественными свойствами.<br>
+        В самом разделе ДЗ реализованы страница со списком врачей, карточка врача с перечнем
+        его процедур, а также формы добавления врачей, процедур и связей между ними. <br>
     </p>
+</div>
+
+<div class="mb-4">
+    <h2><a href="/local/homeworks/homework3/doctors.php">Страница с демонстрацией функционала &rarr;</a></h2>
 </div>
 
 <?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
